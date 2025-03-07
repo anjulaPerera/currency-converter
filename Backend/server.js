1a61c3988ae1ebe6fb20ea46701f0ff4f0c50bb3
Anjula Perera <anjulaperera98@gmail.com>
2025-03-07 17:33:15 +0530

    Initial Commit

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const transferRoutes = require("./routes/transfers");
+app.use("/api/transfers", transferRoutes);
